75d5d369981c95d9e262d8f4ee1a469661afd64d
eghizio <eghizio@gmail.com>
2021-12-06 15:20:20 +0100

    Functional array utils

--- utils/utils.ts
+++ utils/utils.ts
@@ -8,3 +8,9 @@ export const loadInput = (file: string = "input") => {
 
     return input;
 };
+
+export const add = (a: number) => (b: number) => a+b;
+
+export const update = <T>(index: number, cb: (item: T) => T, arr: T[]) => {
+    return arr.map((el, i) => i === index ? cb(el) : el);
+}
